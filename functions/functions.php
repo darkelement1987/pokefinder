ccb5fb73004ef0fd189f2343be6b42f601245d7f
darkelement1987 <jgernaat1987@gmail.com>
2019-10-05 15:11:11 +0200

    Add files via upload

---
+++ functions/functions.php
@@ -0,0 +1,182 @@
+<?php
+function getMons()
+{
+    global $conn;
+    global $assetRepo;
+    global $monsters;
+    
+    $mons = [];
+    $mon_name = json_decode(file_get_contents(DIRECTORY . '/json/pokedex.json'), true);
+    
+    if(!empty($_POST['gender'])){
+        switch ($_POST['gender']) {
+            case '1':
+            $gender = ' IS NOT NULL';
+                break;
+            case '2':
+            $gender = '=1';
+                break;
+            case '3':
+            $gender = '=2';
+                break;
+            case '4':
+            $gender = '=3';
+                break;
+                }
+    } else { $gender = '';}
+    
+    if(!empty($_POST['monster'])){ $monsterid = 'pokemon_id IN (' . $_POST['monster'] . ') AND '; 
+    } elseif(!empty($_POST['generation'])){
+        switch ($_POST['generation']) {
+            case '1':
+            $monsterid = 'pokemon_id BETWEEN 1 AND 151 AND ';
+                break;
+            case '2':
+            $monsterid = 'pokemon_id BETWEEN 152 AND 251 AND ';
+                break;
+            case '3':
+            $monsterid = 'pokemon_id BETWEEN 252 AND 386 AND ';
+                break;
+            case '4':
+            $monsterid = 'pokemon_id BETWEEN 387 AND 493 AND ';
+                break;
+            case '5':
+            $monsterid = 'pokemon_id BETWEEN 494 AND 649 AND ';
+                break;
+                }
+    } else { $monsterid = '';}
+    
+    if(!empty($_POST['boosted'])){ switch ($_POST['boosted']){
+        case'1':
+        $boosted = ' AND weather_boosted_condition=1';
+            break;
+        case'2':
+        $boosted = ' AND weather_boosted_condition=2';
+            break;
+        case'3':
+        $boosted = ' AND weather_boosted_condition=3';
+            break;
+        case'4':
+        $boosted = ' AND weather_boosted_condition=4';
+            break;
+        case'5':
+        $boosted = ' AND weather_boosted_condition=5';
+            break;
+        case'6':
+        $boosted = ' AND weather_boosted_condition=6';
+            break;
+        case'7':
+        $boosted = ' AND weather_boosted_condition=7';
+            break;
+        case'8':
+        $boosted = ' AND weather_boosted_condition BETWEEN 1 AND 7';
+            break;
+            }
+            } else { $boosted = '';}
+    
+    $sql = "SELECT form, gender, catch_prob_1, catch_prob_2, catch_prob_3, cp_multiplier, individual_attack, individual_defense, individual_stamina, pokemon_id, cp, weather_boosted_condition, UNIX_TIMESTAMP(CONVERT_TZ(disappear_time, '+00:00', @@global.time_zone)) as disappear_time, UNIX_TIMESTAMP(CONVERT_TZ(last_modified, '+00:00', @@global.time_zone)) as last_modified, latitude, longitude  FROM pokemon WHERE " . $monsterid . "gender" . $gender . $boosted . " AND disappear_time > utc_timestamp();";
+
+    $result = $conn->query($sql);
+
+    // Check if mon available
+    if ($result && $result->num_rows > 0) {
+        while ($row = $result->fetch_object()) {
+
+            // Pull Mon ID
+            $row->id = '#' . str_pad($row->pokemon_id, 3, 0, STR_PAD_LEFT);
+
+            // Check if mon has stats
+            if ($row->individual_attack !== null && $row->individual_defense !== null &&  $row->individual_stamina !== null) {
+                $row->iv = round((($row->individual_attack + $row->individual_defense + $row->individual_stamina) / 45) * 100, 2);
+                $row->ivoutput = $row->iv . '%';
+                $row->catch_prob_1 = '<img height=\'42\' width=\'42\' src=\'images/poke.png\'>' . round(($row->catch_prob_1) * 100,1) . '% / ';
+                $row->catch_prob_2 = '<img height=\'42\' width=\'42\' src=\'images/great.png\'>' . round(($row->catch_prob_2) * 100,1) . '% / ';
+                $row->catch_prob_3 = '<img height=\'42\' width=\'42\' src=\'images/ultra.png\'>' . round(($row->catch_prob_3) * 100,1) . '%';
+
+            // If no stats show -
+            } else {
+                $row->iv = '-';
+                $row->ivoutput = '-';
+                $row->cp = '-';
+                $row->catch_prob_1 = '-';
+                $row->catch_prob_2 = '';
+                $row->catch_prob_3 = '';
+                $row->individual_attack = '-';
+                $row->individual_defense = '-';
+                $row->individual_stamina = '-';
+            }
+
+            // Detect Gender
+            switch ($row->gender) {
+                case '0':
+                    $row->gender = 'Not Set';
+                    break;
+                case '1':
+                    $row->gender = 'Male';
+                    break;
+                case '2':
+                    $row->gender = 'Female';
+                    break;
+                case '3':
+                    $row->gender = 'Genderless';
+                    break;
+                default:
+                    $row->gender = '-';
+                    break;
+                    }
+            
+            // Detect Weatherboost
+            switch ($row->weather_boosted_condition) {
+                case'0':
+                $row->weather_boosted_condition = '-';
+                    break;
+                case'1':
+                $row->weather_boosted_condition = 'Clear';
+                    break;
+                case'2':
+                $row->weather_boosted_condition = 'Rainy';
+                    break;
+                case'3':
+                $row->weather_boosted_condition = 'Partly  Cloudy';
+                    break;
+                case'4':
+                $row->weather_boosted_condition = 'Cloudy';
+                    break;
+                case'5':
+                $row->weather_boosted_condition = 'Windy';
+                    break;
+                case'6':
+                $row->weather_boosted_condition = 'Snow';
+                    break;
+                case'7':
+                $row->weather_boosted_condition = 'Fog';
+                    break;
+                    }
+
+            // Detect Level
+            if (empty($row->cp_multiplier)){
+                $row->level='-';
+                } else {
+                    if ($row->cp_multiplier < 0.73) {
+                        $row->level = 58.35178527 * $row->cp_multiplier * $row->cp_multiplier - 2.838007664 * $row->cp_multiplier + 0.8539209906;
+                        } elseif ($row->cp_multiplier > 0.73) {
+                            $row->level = 171.0112688 * $row->cp_multiplier - 95.20425243;
+                            }
+                            $row->level = (round($row->level)*2)/2;
+                            }
+
+            $row->sprite = $assetRepo . 'pokemon_icon_' . str_pad($row->pokemon_id, 3, 0, STR_PAD_LEFT) . '_00.png';
+            $row->name = $mon_name[$row->pokemon_id]['name'];
+
+            // Detect Form
+            if (empty($row->form)){
+                $row->form='-';
+                } else {
+                    $row->form = $mon_name[$row->pokemon_id]['forms'][$row->form]['formName'];
+                    }
+
+            $mons[] = $row;
+        }
+        return $mons;
+    }
+}
---
+++ pages/pokefinder.php
@@ -0,0 +1,268 @@
+<?php
+$mons = getMons();
+global $clock;
+?>
+
+<form action="index.php#found" method="post" name="searchmon" id="searchmon" <?php if($_POST){?>hidden<?php }?>><input name="page" type="hidden" value="test" />
+<table style="border-style:solid; border-width:1px; border-color:Gainsboro; margin-left:10px;" cellpadding="5">
+<tbody>
+
+<style>.select2-container {
+width: 100% !important;
+}</style>
+<script>
+
+$(document).ready(function() { 
+        $('.monfind').select2({ width: 'resolve' });           
+});
+
+$(document).ready(function() {
+    $('.monfind').select2();
+      
+});
+
+$(window).bind("pageshow", function() {
+document.getElementById("searchmon").reset();
+});
+</script>
+<tr>
+<td>POKEMON</td>
+<td>
+<script type="text/javascript" language="javascript">
+function toggleSelect()
+{
+
+if (document.getElementById("findgen").checked == false && document.getElementById("findall").checked == false)
+{
+document.searchmon.findall.disabled = false;
+document.searchmon.monster.disabled = false;
+document.searchmon.generation.disabled = true;
+};
+
+if (document.getElementById("findgen").checked == true && document.getElementById("findall").checked == false)
+{
+document.searchmon.findall.disabled = true;
+document.searchmon.monster.disabled = true;
+document.searchmon.generation.disabled = false;
+};
+
+if (document.getElementById("findgen").checked == false && document.getElementById("findall").checked == true)
+{
+document.searchmon.findall.disabled = false;
+document.searchmon.findgen.disabled = true;
+document.searchmon.monster.disabled = true;
+document.searchmon.generation.disabled = true;
+} else {
+    document.searchmon.findgen.disabled = false;
+};
+
+if (document.getElementById("findboost").checked == true)
+{
+document.searchmon.boosted.disabled = false;
+} else {
+    document.searchmon.boosted.disabled = true;
+};
+
+}
+</script>
+<select class="monfind" id="monster" name="monster" style="width:100%;" onChange="monNameFunction(this);" <?php if($_POST){?>disabled<?php }?>>
+<script>
+let dropdown = $('#monster');
+
+dropdown.empty();
+
+dropdown.append('<option selected="true" disabled>Pokemon</option>');
+dropdown.prop('selectedIndex', 0);
+
+const url = './json/dex.json';
+
+$.getJSON(url, function (data) {
+  $.each(data, function (key, entry) {
+    dropdown.append($('<option></option>').attr('value', entry.id).text(entry.name.english));
+  })
+});
+
+</script>
+</select>
+</td>
+<td></td>
+</tr>
+
+<tr>
+<td></td>
+<td><input type="checkbox" id="findall" name="findall" onchange="toggleSelect()" value="1" <?php if($_POST){?>disabled<?php }?> /> ALL POKEMON</td>
+<td></td>
+</tr>
+
+<tr>
+<td>GENERATION</td>
+<td><select name="generation" id="generation" disabled>
+  <option value="1">Gen 1</option>
+  <option value="2">Gen 2</option>
+  <option value="3">Gen 3</option>
+  <option value="4">Gen 4</option>
+  <option value="5">Gen 5</option>
+</select></td>
+<td></td>
+</tr>
+
+<tr>
+<td></td>
+<td><input type="checkbox" id="findgen" name="findgen" onchange="toggleSelect()" value="1" <?php if($_POST){?>disabled<?php }?> /> BY GENERATION</td>
+<td></td>
+</tr>
+
+<tr>
+<td>MIN IV</td>
+<td><input maxlength="3" name="iv" size="2" type="number" min="0" max="100" <?php if($_POST){?>disabled<?php }?> /> %</td>
+<td></td>
+</tr>
+
+<tr>
+<td>MIN CP</td>
+<td><input maxlength="4" name="cp" size="4" type="number" min="1" max="4431" <?php if($_POST){?>disabled<?php }?> /></td>
+<td></td>
+</tr>
+
+<tr>
+<td>MIN LVL</td>
+<td><input maxlength="2" name="lvl" size="2" type="number" min="1" max="35" <?php if($_POST){?>disabled<?php }?> /></td>
+<td></td>
+</tr>
+
+<tr>
+<td>BOOSTED BY</td>
+<td><select name="boosted" disabled>
+  <option value="1">Clear</option>
+  <option value="2">Rainy</option>
+  <option value="3">Partly Cloudy</option>
+  <option value="4">Cloudy</option>
+  <option value="5">Windy</option>
+  <option value="6">Snow</option>
+  <option value="7">Fog</option>
+  <option value="8">Any Weather</option>
+</select></td>
+<td></td>
+</tr>
+
+<tr>
+<td></td>
+<td><input type="checkbox" id="findboost" name="findboost" onchange="toggleSelect()" value="1" <?php if($_POST){?>disabled<?php }?> /> BY BOOST</td>
+<td></td>
+</tr>
+
+<tr>
+<td>GENDER</td>
+<td><select name="gender" <?php if($_POST){?>disabled<?php }?>>
+  <option value="1">All</option>
+  <option value="2">Male</option>
+  <option value="3">Female</option>
+  <option value="4">Genderless</option>
+</select></td>
+<td></td>
+</tr>
+
+<tr>
+<td></td>
+<td><input type="submit" value="Find" style="float:right" <?php if($_POST){?>disabled<?php }?>/></td>
+<td></td>
+</tr>
+
+</tbody>
+</table>
+</form>
+
+<script>
+function monNameFunction(element) {
+    var text = element.options[element.selectedIndex].text;
+    document.getElementById("found").innerHTML = "Your choice: " + text;
+}
+</script>
+
+<p id="found"></p>   
+
+<?php if(!empty($_POST)){?><input type="button" value="Go back!" onclick="history.back();"><?php } ?><br>
+
+<table id="mon_table" class="table table-striped table-bordered" style="width:100%" <?php if(!$_POST){?>hidden<?php }?>>
+    <thead>
+        <tr>
+            <th style="display:none";>ID:</th>
+            <th>Pokemon:</th>
+            <th>IV:</th>
+            <th>CP:</th>
+            <th>Boosted by:</th>
+            <th>Level:</th>
+            <th>Gender:</th>
+            <th>Form:</th>
+            <th>Att:</th>
+            <th>Def:</th>
+            <th>Sta:</th>
+            <th>Catch Rate:</th>
+            <th>Disappears:</th>
+            <th>Scanned:</th>
+            <th>Google Maps:</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php if (is_array($mons)) {
+            foreach ($mons as $row) {
+                if(isset($_POST['iv'])){$miniv = $_POST['iv'];} else {$miniv = '0';}
+                if(isset($_POST['cp'])){$mincp = $_POST['cp'];} else {$mincp = '0';}
+                if(isset($_POST['lvl'])){$minlvl = $_POST['lvl'];} else {$minlvl = '0';}
+                if(isset($_POST['name'])){$monname = $_POST['name'];} else {$monname = '0';}
+                if($row->iv >= $miniv && $row->cp >= $mincp && $row->level >= $minlvl){
+                ?>
+                <tr>
+                    <td style="display:none";><?= $row->id ?></td>
+                    <td><img height='42' width='42' src='<?= $row->sprite ?>'/> <?= $row->name ?></td>
+                    <td><?= $row->ivoutput ?></td>
+                    <td><?= $row->cp ?></td>
+                    <td><?= $row->weather_boosted_condition ?></td>
+                    <td><?= $row->level ?></td>
+                    <td><?= $row->gender ?></td>
+                    <td><?= $row->form ?></td>
+                    <td><?= $row->individual_attack ?></td>
+                    <td><?= $row->individual_defense ?></td>
+                    <td><?= $row->individual_stamina ?></td>
+                    <td><?= $row->catch_prob_1 ?><?= $row->catch_prob_2 ?><?= $row->catch_prob_3 ?></td>
+                    <td><?= date($clock, $row->disappear_time) ?></td>
+                    <td><?= date($clock, $row->last_modified) ?></td>
+                    <td><a href='https://maps.google.com/?q=<?= $row->latitude ?>,<?= $row->longitude ?>'>MAP</a></td>
+        </tr> <?php }}
+                        } else {
+                            echo $mons;
+                        } ?> </tbody>
+</table>
+<?php if(!empty($_POST)){?>
+<script>
+    $(document).ready(function() {
+        $('#mon_table').DataTable({
+            order: [
+                [13, "desc"]
+                ],
+
+            columnDefs: [
+            { type: 'time-uni', targets: 12 },
+            { type: 'time-uni', targets: 13 },
+            { "targets": [ 0 ], "visible": false}
+            ],
+
+            paging: true,
+            lengthChange: false,
+            searching: true,
+            responsive: true,
+            
+            language: {
+                "search":         "Filter results:",
+                "info":           "Showing _START_ to _END_ of _TOTAL_ Pokémon",
+                "infoEmpty":      "Showing 0 to 0 of 0 Pokémon",
+                "infoFiltered":   "(filtered from _MAX_ total Pokémon)",
+                "emptyTable":     "No Pokémon available in table",
+                "zeroRecords":    "No matching Pokémon found",
+                searchPlaceholder: "Enter name/id"
+                }
+            
+        });
+    });
+</script>
+<?php }?>
