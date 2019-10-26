be8c7ef988e54ae184819c72b98cb69376b67423
darkelement1987 <jgernaat1987@gmail.com>
2019-10-26 14:53:48 +0200

    Solve UTF8 problems, Next: Merge :eyes:

--- includes.php
+++ includes.php
@@ -4,6 +4,7 @@ define('DIRECTORY', __Dir__);
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
+$conn->set_charset('utf8');
 
 // Check connection
 if ($conn->connect_error) {
