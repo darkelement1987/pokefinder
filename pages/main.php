010488302d7a5e1f6b4300a21920ba47d5663a81
darkelement1987 <jgernaat1987@gmail.com>
2019-10-27 15:30:02 +0100

    Update main.php

--- pages/main.php
+++ pages/main.php
@@ -7,36 +7,31 @@ global $title;
 ?>
 <!-- START OF MAP RAME -->
 <div class="mainpage">
-<div class="container mt-3">
+<div class="container">
   <h2><?= $title?></h2>
   <p>Activity in your area:</p>
-  <div class="media border p-3">
+  <div class="media border p-3 m-1">
     <img src="https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/pokeball_sprite.png" class="mr-3 mt-3 rounded-circle" style="width:60px; height:60px;">
     <div class="media-body">
       <p><h4><strong>Pokemon</strong></h4>
       <h5>Available: <span class="badge badge-success"><?= $moncount ?></span></h5></p>   
     </div>
   </div>
-</div>
-<div class="container mt-3">
-  <div class="media border p-3">
+  <div class="media border p-3 m-1">
     <img src="https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/decrypted_assets/png/m_hat_teamrocket_bundle_icon.png" class="mr-3 mt-3 rounded-circle" style="width:60px; height:60px;">
     <div class="media-body">
       <p><h4><strong>Team Rocket</strong></h4>
       <h5>Available: <span class="badge badge-success"><?= $stopcount ?></span></h5></p>   
     </div>
   </div>
-<div class="container mt-3">
-  <div class="media border p-3">
+  <div class="media border p-3 m-1">
     <img src="https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/QuestIconProfessor.png" class="mr-3 mt-3 rounded-circle" style="width:60px; height:60px;">
     <div class="media-body">
       <p><h4><strong>Quests</strong></h4>
       <h5>Available: <span class="badge badge-success"><?= $questcount ?></span></h5></p>   
     </div>
   </div>
-</div>
-<div class="container mt-3">
-  <div class="media border p-3">
+  <div class="media border p-3 m-1">
     <img src="https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/static_assets/png/tx_raid_coin.png" class="mr-3 mt-3 rounded-circle" style="width:60px; height:60px;">
     <div class="media-body">
       <p><h4><strong>Raids</strong></h4>
