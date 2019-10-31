e865c7584ea208f4ce5881792882b5d8ce0ec017
darkelement1987 <jgernaat1987@gmail.com>
2019-10-31 16:53:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ include './includes.php';
     <body>
         <div class="topnav" id="myTopnav">
             <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark shadow-lg p-30 mb-30">
-                <a class="navbar-brand" href="index.php"><img src="images/locamon.png" alt="Logo" class="logo"> PokeFinder</a>
+                <a class="navbar-brand" href="index.php"><img src="images/locamon.png" alt="Logo" class="logo"> <?= $title?></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
