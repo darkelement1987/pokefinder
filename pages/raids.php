fd2f2b81ae81c0b344937d6bf594b5f78ebe548d
darkelement1987 <jgernaat1987@gmail.com>
2019-10-31 07:36:55 +0100

    Add search for team name to raid page

--- pages/raids.php
+++ pages/raids.php
@@ -27,7 +27,7 @@ global $clock;
                 ?>
                 <tr>
                     <td></td>
-                    <td><img class='<?= $row->team?>' height='42' width='42' src='<?= $row->image ?>' title='Gym control: <?= $row->team?>'/></td>
+                    <td><span hidden><?= $row->team?></span><img class='<?= $row->team?>' height='42' width='42' src='<?= $row->image ?>' title='Gym control: <?= $row->team?>'/></td>
                     <td><a href='https://www.google.com/maps?q=<?= $row->latitude?>,<?= $row->longitude ?>'><?= $row->name ?></a></td>
                     <td><?= $row->bossname ?></td>
                     <td><?= $row->move_1 . $row->move_2 ?></td>
