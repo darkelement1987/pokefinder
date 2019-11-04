b6c07f39d7bb0f57ee291c1a8f9d495bfe0d853a
darkelement1987 <jgernaat1987@gmail.com>
2019-11-05 00:36:02 +0100

    Grey out dex entries if unseen

--- pages/pokedex.php
+++ pages/pokedex.php
@@ -1,5 +1,6 @@
 <?php
 global $assetRepo;
+global $conn;
 
 $url = 'https://raw.githubusercontent.com/KartulUdus/Professor-Poracle/master/src/util/description.json'; // path to your JSON file
 $data = file_get_contents($url); // put the contents of the file into a variable
@@ -9,10 +10,15 @@ $characters = json_decode($data); // decode the JSON feed
   <div class="row" id="pokedex">
   <h3>Pokedex</h3>
   <input class="form-control" id="myInput" type="text" placeholder="Search.."><br>
-<?php foreach ($characters as $character) {?>  
+<?php foreach ($characters as $character) {
+    $monquery = $conn->query("select count(*) as count from pokemon where pokemon_id=" . str_pad($character->pkdx_id, 1, 0, STR_PAD_LEFT));
+    $monrow = $monquery->fetch_assoc();
+    $monquery->close();
+    $count=$monrow['count'];
+    ?>  
     <div class="col">
 <center>
-<a href="index.php?page=seen&pokemon=<?php echo str_pad($character->pkdx_id, 1, 0, STR_PAD_LEFT)?>"><img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/<?php echo str_pad($character->pkdx_id, 3, 0, STR_PAD_LEFT)?>.png" class="dexentry"></a>
+<a href="index.php?page=seen&pokemon=<?php echo str_pad($character->pkdx_id, 1, 0, STR_PAD_LEFT)?>"><img <?php if ($count <1){?>class="unseen" <?php }?>src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/<?php echo str_pad($character->pkdx_id, 3, 0, STR_PAD_LEFT)?>.png" class="dexentry"></a>
 <br>
 <?php echo $character->name;?></center>
 </div>
