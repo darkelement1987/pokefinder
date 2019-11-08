cd5d508c0152f8cd6e54da50c280c1b5542b30c8
darkelement1987 <jgernaat1987@gmail.com>
2019-11-08 19:03:56 +0100

    Self-host mon images

--- pages/pokedex.php
+++ pages/pokedex.php
@@ -35,13 +35,16 @@ if($formresult && $formresult->num_rows >= 1 ) {
     $monid = str_pad($entry->id, 1, 0, STR_PAD_LEFT);
     $monsprite = str_pad($entry->id, 3, 0, STR_PAD_LEFT);
     if(!empty($entry->form->name)){$monname = $entry->name . ' (' . $entry->form->name . ')'; $formid = '_' . $entry->form->id;} else {$monname = $entry->name; $formid = '_00';}
-    $imgurl = 'https://raw.githubusercontent.com/whitewillem/PogoAssets/resized/no_border/pokemon_icon_' . $monsprite . $formid . '.png';
+    $imgurl = 'images/pokemon/pokemon_icon_' . $monsprite . $formid . '.png';
+    if(!file_exists($imgurl)){
+        $imgurl='https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/pokemon_icons/pokemon_icon_000.png';
+    }
     ?>  
 <?php if($entry->form->name != 'Purified' && $entry->form->name != 'Shadow' && $entry->form->name != 'Normal'){?>
 <div class="col">
 <center>
 <?php if($entry->form->name == ''){?><a href="index.php?page=seen&pokemon=<?= $monid?>"><?php } else {?><a href="index.php?page=seen&pokemon=<?= $monid?>&form=<?= $entry->form->id?>"><?php }?>
-<img <?php if (!in_array($monid, $seen) || !in_array($entry->form->id, $formseen)) {?>class="unseen"<?php } else {?>class="dexentry"<?php }?>src="<?=$imgurl?>" onerror="this.src='https://raw.githubusercontent.com/ZeChrales/PogoAssets/master/pokemon_icons/pokemon_icon_000.png'">
+<img <?php if (!in_array($monid, $seen) || !in_array($entry->form->id, $formseen)) {?>class="unseen"<?php } else {?>class="dexentry"<?php }?>src="<?=$imgurl?>">
 </a>
 <br>
 <?= $monname?></center>
