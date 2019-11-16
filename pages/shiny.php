a65a2d180586ab3502bd110b967840c6ec9b30a2
darkelement1987 <jgernaat1987@gmail.com>
2019-11-16 11:04:49 +0100

    Make shiny disclaimer dismissable on shiny-page

--- pages/shiny.php
+++ pages/shiny.php
@@ -7,7 +7,12 @@ $query = "select pokemon.pokemon_id, pokemon.disappear_time, pokemon.latitude, p
 $result = $conn->query($query);?>
 <h3>Shinies found</h3>
 <div class="table-responsive-sm">
-<div class="alert alert-danger" role="alert">Disclaimer: These Pokemon are not shiny guaranteed!</div>
+<div class="alert alert-danger alert-dismissible fade show" role="alert" style="display:inline-block;">
+  <strong>Disclaimer:</strong> These Pokemon are not shiny guaranteed!
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
 <table id="shinyTable" class="table table-striped table-bordered w-auto display compact">
   <thead>
     <tr>
