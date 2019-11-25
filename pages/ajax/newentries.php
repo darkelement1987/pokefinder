0cfc33eb3ca650ce3b3b1d2a0b31826ff6703ba4
darkelement1987 <jgernaat1987@gmail.com>
2019-11-25 03:08:01 +0100

    Update newentries.php

--- pages/ajax/newentries.php
+++ pages/ajax/newentries.php
@@ -12,8 +12,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-include '../../functions/functions.php';
-
 switch ($seconds) {
     case true:
         $showseconds = ':s';
@@ -37,6 +35,8 @@ switch ($clock) {
         $clock = 'g:i' . $showseconds;
         break;
 }
+
+include '../../functions/functions.php';
 
 $monname = json_decode(file_get_contents('https://raw.githubusercontent.com/cecpk/OSM-Rocketmap/master/static/data/pokemon.json'), true);
 $query = "select pokemon_id, form, UNIX_TIMESTAMP(CONVERT_TZ(last_modified, '+00:00', @@global.time_zone)) as last_modified, UNIX_TIMESTAMP(CONVERT_TZ(disappear_time, '+00:00', @@global.time_zone)) as disappear_time, count(pokemon.pokemon_id) as count from pokemon group by pokemon_id, form having count <= 1 order by disappear_time desc";
