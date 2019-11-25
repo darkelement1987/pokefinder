9b01700a0e91dd35970faacc986f844d060de34a
darkelement1987 <jgernaat1987@gmail.com>
2019-11-25 02:58:02 +0100

    changes

--- pages/ajax/newentries.php
+++ pages/ajax/newentries.php
@@ -14,8 +14,29 @@ if ($conn->connect_error) {
 
 include '../../functions/functions.php';
 
-global $conn;
-global $clock;
+switch ($seconds) {
+    case true:
+        $showseconds = ':s';
+        break;
+    case false:
+        $showseconds = '';
+        break;
+    default:
+        $showseconds = '';
+        break;
+}
+
+switch ($clock) {
+    case '24':
+        $clock = 'H:i' . $showseconds;
+        break;
+    case '12':
+        $clock = 'g:i' . $showseconds;
+        break;
+    default:
+        $clock = 'g:i' . $showseconds;
+        break;
+}
 
 $monname = json_decode(file_get_contents('https://raw.githubusercontent.com/cecpk/OSM-Rocketmap/master/static/data/pokemon.json'), true);
 $query = "select pokemon_id, form, UNIX_TIMESTAMP(CONVERT_TZ(last_modified, '+00:00', @@global.time_zone)) as last_modified, UNIX_TIMESTAMP(CONVERT_TZ(disappear_time, '+00:00', @@global.time_zone)) as disappear_time, count(pokemon.pokemon_id) as count from pokemon group by pokemon_id, form having count <= 1 order by disappear_time desc";
