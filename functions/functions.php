87bd0d733541c5b704bfbcce6093c87ef54f492e
darkelement1987 <jgernaat1987@gmail.com>
2019-11-28 01:02:51 +0100

    Remove unused quests-function

--- functions/functions.php
+++ functions/functions.php
@@ -236,43 +236,6 @@ function getRocket()
                                                 return $rocket;
                                                 }
                                                 }
-                                                
-function getQuest()
-{
-    global $conn;
-
-    $quest = [];
-
-    $sql = "SELECT pokestop.latitude as lat, pokestop.longitude as lon, pokestop.name, pokestop.image, trs_quest.quest_reward_type as type, trs_quest.quest_item_amount as amount, trs_quest.quest_task as task, trs_quest.quest_stardust as stardust, trs_quest.quest_pokemon_id as monid, trs_quest.quest_item_id as itemid from pokestop,trs_quest WHERE trs_quest.GUID = pokestop.pokestop_id;";
-    $result = $conn->query($sql);
-    $mon_name = json_decode(file_get_contents('https://raw.githubusercontent.com/cecpk/OSM-Rocketmap/master/static/data/pokemon.json'), true);
-    $item_name = json_decode(file_get_contents('https://raw.githubusercontent.com/whitewillem/PMSF/master/static/data/items.json'), true);
-
-    if ($result && $result->num_rows > 0){
-        while ($row = $result->fetch_object()) {
-            $row->text='';
-            $row->monname='';
-            $row->item='';
-            switch ($row->type) {
-                case '2':
-                $row->item = $item_name[$row->itemid]['name'];
-                $row->type = 'https://raw.githubusercontent.com/cecpk/OSM-Rocketmap/master/static/images/quest/reward_' . $row->itemid . '_1.png';
-                $row->text = $row->amount;
-                break;
-                case '3':
-                $row->type = 'https://raw.githubusercontent.com/Map-A-Droid/MAD/master/madmin/static/quest/reward_stardust.png';
-                $row->text = $row->stardust . ' Stardust';
-                break;
-                case '7':
-                $row->type = monPic('pokemon',$row->monid,0);
-                $row->text = '<br><a href="index.php?page=seen&pokemon=' . $row->monid . '">' . $mon_name[$row->monid]['name'] . '</a>';
-                break;
-            }
-            $quest[] = $row;
-                }
-                return $quest;
-                }
-                }
 
 function getRaids()
 {
